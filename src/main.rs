0b5a8c6ffc004be2f6c466c0c341a187022f85e7
Maksymilian Jankowski <ma.ad.jank@gmail.com>
2025-12-11 14:49:50 +0000

    updates to make the drawing nice

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,61 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use std::error::Error;
+use std::io::{Stdout, Write, stdout};
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    let secret_number = rand::rng().random_range(1..=100);
+use crossterm::{
+    ExecutableCommand, cursor,
+    terminal::{self, Clear, ClearType},
+};
 
-    println!("The seecret number is {secret_number}");
+fn draw_game(stdout: &mut Stdout) -> Result<(), Box<dyn Error>> {
+    let wsize = terminal::window_size().expect("I mean bruh, we need the window size eh?");
 
-    println!("Enter and guess a number!");
+    if wsize.rows < 30 || wsize.columns < 30 {
+        stdout.execute(Clear(ClearType::All))?;
 
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Something went wrong");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("You can only enter numbers!");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too little!"),
-            Ordering::Equal => {
-                println!("Nice, you guessed");
-                break;
-            }
-            Ordering::Greater => println!("Too big!"),
-        }
+        stdout.execute(cursor::MoveTo(0, 0))?;
+        write!(stdout, "Your terminal is too little dude")?;
+        return Result::Ok(());
     }
+
+    stdout.execute(Clear(ClearType::All))?;
+
+    stdout.execute(cursor::MoveTo(0, 0))?;
+    let rows = wsize.rows;
+    let columns = wsize.columns;
+    write!(stdout, "{rows}")?;
+
+    stdout.execute(cursor::MoveTo(0, 1))?;
+    write!(stdout, "{columns}")?;
+
+    stdout.flush()?;
+
+    thread::sleep(Duration::from_millis(150));
+
+    Ok(());
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdout = stdout();
+
+    // Enter raw mode and hide cursor
+    terminal::enable_raw_mode()?;
+    stdout.execute(cursor::Hide)?;
+
+    // Each frame is a list of lines
+    let frames: Vec<Vec<&str>> = vec![
+        vec!["   .-.", "  (o o)", "  | O \\", "   \\   \\", "    `~~~`"],
+        vec!["   .-.", "  (o o)", "  | O )", "   /  /", "    `~~~`"],
+    ];
+
+    for i in 0..10 {
+        draw_game(&mut stdout);
+    }
+
+    // Show cursor again and disable raw mode before exiting
+    stdout.execute(cursor::Show)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
