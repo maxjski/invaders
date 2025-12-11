7586f71cab01c4a0bb175e8f0827b3d833fb5291
Maksymilian Jankowski <ma.ad.jank@gmail.com>
2025-12-11 16:12:49 +0000

    refactor so draw is implemented on game_state

--- src/main.rs
+++ src/main.rs
@@ -60,53 +60,28 @@ fn render_borders(wsize: &WindowSize, stdout: &mut Stdout) -> Result<(), Box<dyn
         stdout.execute(cursor_to_top_left)?;
         write!(stdout, "#")?;
     }
+    stdout.execute(cursor::MoveTo(
+        wsize.columns / 2 + SCREEN_WIDTH / 2,
+        wsize.rows / 2 + SCREEN_HEIGHT / 2,
+    ))?;
+    write!(stdout, "#")?;
+    stdout.flush()?;
 
     Ok(())
 }
 
-fn draw_game(game_state: &mut GameState) -> Result<(), Box<dyn Error>> {
-    if game_state.wsize_updated {
-        game_state.wsize_updated = false;
+impl GameState {
+    fn draw_game(&mut self) -> Result<(), Box<dyn Error>> {
+        if self.wsize_updated {
+            self.wsize_updated = false;
 
-        render_borders(&game_state.wsize, &mut game_state.stdout)?;
+            render_borders(&self.wsize, &mut self.stdout)?;
+        }
+
+        thread::sleep(Duration::from_millis(150));
+
+        Ok(())
     }
-
-    // let stdout = &mut game_state.stdout;
-    // let wsize = terminal::window_size().expect("I mean bruh, we need the window size eh?");
-    //
-    // if wsize.rows < 50 || wsize.columns < 50 {
-    //     stdout.execute(Clear(ClearType::All))?;
-    //
-    //     stdout.execute(cursor::MoveTo(0, 0))?;
-    //     write!(stdout, "Your terminal is too little dude")?;
-    //     return Result::Ok(());
-    // }
-    //
-    // stdout.execute(Clear(ClearType::All))?;
-    //
-    // for i in 0..40 {
-    //     let cursor_to_top_left = cursor::MoveTo(wsize.columns / 2 - 20 + i, wsize.rows / 2 - 20);
-    //     stdout.execute(cursor_to_top_left)?;
-    //     write!(stdout, "#")?;
-    //
-    //     let cursor_to_top_left = cursor::MoveTo(wsize.columns / 2 + 20, wsize.rows / 2 - 20 + i);
-    //     stdout.execute(cursor_to_top_left)?;
-    //     write!(stdout, "#")?;
-    //
-    //     let cursor_to_top_left = cursor::MoveTo(wsize.columns / 2 - 20, wsize.rows / 2 - 20 + i);
-    //     stdout.execute(cursor_to_top_left)?;
-    //     write!(stdout, "#")?;
-    //
-    //     let cursor_to_top_left = cursor::MoveTo(wsize.columns / 2 - 20 + i, wsize.rows / 2 + 20);
-    //     stdout.execute(cursor_to_top_left)?;
-    //     write!(stdout, "#")?;
-    // }
-    //
-    // stdout.flush()?;
-
-    thread::sleep(Duration::from_millis(150));
-
-    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -117,20 +92,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(cursor::Hide)?;
 
     // Each frame is a list of lines
-    let frames: Vec<Vec<&str>> = vec![
-        vec!["   .-.", "  (o o)", "  | O \\", "   \\   \\", "    `~~~`"],
-        vec!["   .-.", "  (o o)", "  | O )", "   /  /", "    `~~~`"],
-    ];
-
     let mut game_state = GameState {
         wsize_updated: true,
         wsize: terminal::window_size()?,
         stdout,
     };
 
-    for i in 0..10 {
-        draw_game(&mut game_state)?;
-    }
+    game_state.draw_game()?;
 
     // Show cursor again and disable raw mode before exiting
     game_state.stdout.execute(cursor::Show)?;
