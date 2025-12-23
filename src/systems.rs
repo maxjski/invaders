5b5eb5c7c5c1b895eacbac94aeaa448309735d14
Maksymilian Jankowski <2619901j@student.gla.ac.uk>
2025-12-23 00:21:54 +0000

    fix shooting problem

--- src/systems.rs
+++ src/systems.rs
@@ -436,49 +436,49 @@ fn process_coplayer_projectile(
 ) -> Result<(), Box<dyn Error>> {
     spawn_coplayer_projectile(game_state);
 
-    let mut player_projectile: Option<Entity> = Option::None;
-    for (id, (pos, prev_pos, vel, renderable)) in game_state
-        .world
-        .query_mut::<(
-            &mut Position,
-            &mut PrevPosition,
-            &mut Velocity,
-            &mut Renderable,
-        )>()
-        .with::<&PlayerProjectile>()
-    {
-        // projectile sprite was destroyed by renderer
-        if renderable.erased {
-            player_projectile = Option::Some(id);
-        }
-
-        vel.move_accumulator += vel.speed * delta_time.as_secs_f32();
-
-        if vel.move_accumulator >= 1.0 || vel.move_accumulator <= -1.0 {
-            // Move in whole-cell steps, keep fractional remainder to avoid drift and asymmetry
-            let steps = vel.move_accumulator.trunc();
-            let new_pos = pos.y as i32 + steps as i32;
-
-            let old_pos = pos.y;
-            prev_pos.y = old_pos;
-
-            if new_pos < 2 {
-                pos.y = 2;
-            } else if new_pos > 39 {
-                renderable.destroy = true;
-            } else {
-                pos.y = new_pos as u16;
-            }
-
-            vel.move_accumulator -= steps;
-        }
-    }
-
-    // If player projectile is assigned, we need to destory it
-    if let Some(player_projectile) = player_projectile {
-        game_state.world.despawn(player_projectile)?;
-        game_state.player_projectile_exists = false;
-    }
+    // let mut player_projectile: Option<Entity> = Option::None;
+    // for (id, (pos, prev_pos, vel, renderable)) in game_state
+    //     .world
+    //     .query_mut::<(
+    //         &mut Position,
+    //         &mut PrevPosition,
+    //         &mut Velocity,
+    //         &mut Renderable,
+    //     )>()
+    //     .with::<&PlayerProjectile>()
+    // {
+    //     // projectile sprite was destroyed by renderer
+    //     if renderable.erased {
+    //         player_projectile = Option::Some(id);
+    //     }
+    //
+    //     vel.move_accumulator += vel.speed * delta_time.as_secs_f32();
+    //
+    //     if vel.move_accumulator >= 1.0 || vel.move_accumulator <= -1.0 {
+    //         // Move in whole-cell steps, keep fractional remainder to avoid drift and asymmetry
+    //         let steps = vel.move_accumulator.trunc();
+    //         let new_pos = pos.y as i32 + steps as i32;
+    //
+    //         let old_pos = pos.y;
+    //         prev_pos.y = old_pos;
+    //
+    //         if new_pos < 2 {
+    //             pos.y = 2;
+    //         } else if new_pos > 39 {
+    //             renderable.destroy = true;
+    //         } else {
+    //             pos.y = new_pos as u16;
+    //         }
+    //
+    //         vel.move_accumulator -= steps;
+    //     }
+    // }
+    //
+    // // If player projectile is assigned, we need to destory it
+    // if let Some(player_projectile) = player_projectile {
+    //     game_state.world.despawn(player_projectile)?;
+    //     game_state.player_projectile_exists = false;
+    // }
 
     Ok(())
 }
@@ -529,8 +529,19 @@ fn process_player_projectile(
 
     // If player projectile is assigned, we need to destory it
     if let Some(player_projectile) = player_projectile {
+        // Check if it's a coplayer projectile before despawning
+        let is_coplayer_projectile = game_state
+            .world
+            .get::<&CoPlayerProjectile>(player_projectile)
+            .is_ok();
+
         game_state.world.despawn(player_projectile)?;
-        game_state.player_projectile_exists = false;
+
+        if is_coplayer_projectile {
+            game_state.coplayer_handler.projectile_exists = false;
+        } else {
+            game_state.player_projectile_exists = false;
+        }
     }
 
     Ok(())
